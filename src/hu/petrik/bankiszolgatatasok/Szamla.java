9d4e9704982fc5930a19a17a1b441b06ce55dee9
John Doe <johndoe@example.com>
2022-10-14 16:41:15 +0200

    Sok szenvedés eredménye

---
+++ src/hu/petrik/bankiszolgatatasok/Szamla.java
@@ -0,0 +1,22 @@
+package hu.petrik.bankiszolgatatasok;
+
+public abstract class Szamla extends BankiSzolgaltatas{
+    protected int aktualisEgyenleg;
+    public Szamla (Tulajdonos tulajdonos){
+        super(tulajdonos);
+    }
+
+    public int getAktualisEgyenleg(){
+        return this.aktualisEgyenleg;
+    }
+
+    public void befizet(int osszeg){
+        this.aktualisEgyenleg += osszeg;
+    }
+
+    public abstract boolean kivesz(int osszeg);
+
+     public Kartya ujKartya(String kartyaSzam){
+
+    }
+}
